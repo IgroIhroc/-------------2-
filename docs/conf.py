3f7f2f3f5f91dd97379f76adb158ed9c82a72bda
IgroIhroc <holovatyi.illia@chnu.edu.ua>
2026-05-10 22:55:54 +0300

    full finisch!

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ exclude_patterns = []
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_css_files = ['custom.css']
+
 html_theme_options = {
     'collapse_navigation': False,
     'navigation_depth': 4,
